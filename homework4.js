cb18a51fb902ebca9d479f58f83482c8f65cdc09
Gevorg Gevorgyan <gevorgyangev006@gmail.com>
2024-10-16 21:10:06 +0400

    Add files via upload

---
+++ homework4.js
@@ -0,0 +1,45 @@
+//1. Given a positive integer. Bring the last digit of the number to the beginning. Print the new
+//number. If the last digit of the inserted number is 0, number remains the same. 
+
+
+function positiveInteger(a) {
+    let number = a;
+   if (a%10===0) {
+       return a;
+   } else {
+       return a%10 + '' + Math.floor(a/10);
+   }
+}
+
+//2. Insert a digit and a number. Check whether the digits contains in the number or not.
+function fn(digit, number) {
+     let digitOne = number % 10
+   if (digitOne === digit) {
+         return 'yes'; 
+   } else {
+         return 'no';
+   }
+}
+
+
+//3. Enter a number. Reverse its first and last digits. Print the new number.
+
+
+function reverse(a) {
+    let numberA = a;
+     if (a<10) {
+         return a;
+     } else {
+         return a%10 + '' + Math.floor(a/10);
+   }
+}
+
+//4. Enter a number. Find the difference between its biggest and smallest digits.
+
+
+function difference(b) {
+    let numberB = b;
+     
+       
+
+}
